063cb752858789fc1667b69b4c1a16870249d8ef
Carolina Garcia <Caro_ga_mu@hotmail.com>
2018-03-02 20:14:12 -0600

    Ejemplo de lectura de datos
    Ejercicio para aprender bien como se realiza la lectura de datos

--- Lectura de datos 28-02.R
+++ Lectura de datos 28-02.R
@@ -12,5 +12,17 @@ dput(y,file="y.R")
 #4. Genero un nuevo objeto a partir de la informacion guardada
 y2 <- dget(file="y.R")
 
+
+
+
+w <- data.frame(a=1, b="a")
+w
+#2. Estudio la manera en la que se guardara
+dput(w)
+#3. lo guardo en mi disco duro
+dput(w,file="w.R")
+#4. Genero un nuevo objeto a partir de la informacion guardada
+w2 <- dget(file="w.R")
+
 #Tarea almacenar en tu disco duro la base de datos
 #mtcars en un archivo "carros.R" y posteriormente guardalo en un dataframe que se llame carros(tunombre)
