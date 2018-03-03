22fafd121153ef63f6aff1165fde8189e7767461
Carolina Garcia <Caro_ga_mu@hotmail.com>
2018-03-02 20:04:21 -0600

    Lectura de Datos 28-02
    Se nos enseño como funcionan dput y dget para leer datos

---
+++ Lectura de datos 28-02.R
@@ -0,0 +1,16 @@
+#Lectura y escritura de datos 
+
+#dput y dget
+
+#1. Creo el objeto
+y <- data.frame(a=1, b="a")
+y
+#2. Estudio la manera en la que se guardara
+dput(y)
+#3. lo guardo en mi disco duro
+dput(y,file="y.R")
+#4. Genero un nuevo objeto a partir de la informacion guardada
+y2 <- dget(file="y.R")
+
+#Tarea almacenar en tu disco duro la base de datos
+#mtcars en un archivo "carros.R" y posteriormente guardalo en un dataframe que se llame carros(tunombre)
